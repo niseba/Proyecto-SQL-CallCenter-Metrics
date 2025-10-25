2efce38a0bf4259d46a641d5a2c19036250d5719
niseba <banicolas123@gmail.com>
2025-10-25 14:43:00 -0500

    17_Tiempo_Llamadas_Fines_Semana.sql

--- Queries/17_Tiempo_Llamadas_Fines_Semana.sql
+++ Queries/17_Tiempo_Llamadas_Fines_Semana.sql
@@ -2,12 +2,12 @@
 -- Objetivo:  
 -- Muestra el total de tiempo invertido en llamadas por cada cliente durante fines de semana.
 
-SELECT cu.customer_name, cu.customer_id,
-       COUNT(ca.call_id) as Cantidad_Llamadas
-
-FROM calls ca
-JOIN customers cu on cu.customer_id = ca.customer_id
-GROUP BY cu.customer_name, cu.customer_id
-HAVING COUNT(ca.call_id)>=2
-ORDER BY Cantidad_Llamadas DESC;
+SELECT cu.customer_id,
+       cu.customer_name,
+       SUM(ca.duration_minutes) as Tiempo_Total
+       
+FROM customers cu
+JOIN calls ca on ca.customer_id = cu.customer_id
+GROUP BY cu.customer_id, cu.customer_name
+ORDER BY Tiempo_Total DESC;
 
