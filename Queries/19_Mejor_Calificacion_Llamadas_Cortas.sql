b454fe5be68c6f7353dc38e394c3ae58b7da9f14
niseba <banicolas123@gmail.com>
2025-10-25 15:33:14 -0500

    19_Mejor_Calificacion_Llamadas_Cortas.sql

--- Queries/19_Mejor_Calificacion_Llamadas_Cortas.sql
+++ Queries/19_Mejor_Calificacion_Llamadas_Cortas.sql
@@ -3,14 +3,15 @@
 -- Obtener el top 5 de agentes con mejor calificación promedio (rating)
 -- de las encuestas posteriores a llamadas que no hayan durado más de 15 minutos.
 
-SELECT 
-    ag.agent_name, 
-    ROUND(AVG(su.rating), 1) AS Promedio_Calificación
-FROM agents ag
-JOIN calls ca ON ca.agent_id = ag.agent_id
-JOIN surveys su ON su.call_id = ca.call_id
+SELECT ag.agent_id, ag.agent_name,
+       ROUND(AVG(su.rating),1) as Promedio_Calificación
+
+FROM surveys su
+JOIN calls ca on ca.call_id = su.call_id
+JOIN agents ag on ag.agent_id = ca.agent_id
 WHERE ca.duration_minutes <= 15
-GROUP BY ag.agent_name
+GROUP BY ag.agent_id, ag.agent_name
 ORDER BY Promedio_Calificación DESC
 LIMIT 5;
+
 
